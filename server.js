4403485423cd07af4cf5b393a54b308f7835029a
mercer-mk <milanckhunt@gmail.com>
2020-03-02 17:08:23 +0530

    removed initial routes

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.json({ msg: "Welcome " }));
-
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
